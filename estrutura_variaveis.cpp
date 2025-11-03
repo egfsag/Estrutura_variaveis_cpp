03aec3fc6bfe9d73ac58752b5c4fb8fd99dc4af4
egfsag <phezim08@gmail.com>
2025-11-03 16:49:07 -0300

    Create estrutura_variaveis.cpp

---
+++ estrutura_variaveis.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+  int idade = 19;
+  string nome = "Teixeira";
+
+  cout << "O meu nome e: " << nome << " , e minha idade e: " << idade;
+
+  return 0;
+}
